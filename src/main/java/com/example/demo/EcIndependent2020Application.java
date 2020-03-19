0317bae4932a09545ffe33fb6ef765bf3f069b98
nosemasashi <ma.nose9413@gmail.com>
2020-03-19 11:08:33 +0900

    first commit

---
+++ src/main/java/com/example/demo/EcIndependent2020Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcIndependent2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcIndependent2020Application.class, args);
+	}
+
+}
